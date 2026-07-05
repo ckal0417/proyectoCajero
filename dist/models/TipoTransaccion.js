270a7dae26fdf0d287b9fdf73c39b5360b5eccdc
ckal0417 <ckal17042004@gmail.com>
2026-07-05 02:04:03 -0500

    Se agrega gitignore y se eliminan archivos generados

--- dist/models/TipoTransaccion.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TipoTransaccion = void 0;
-var TipoTransaccion;
-(function (TipoTransaccion) {
-    TipoTransaccion["CONSULTA"] = "CONSULTA";
-    TipoTransaccion["DEPOSITO"] = "DEPOSITO";
-    TipoTransaccion["RETIRO"] = "RETIRO";
-})(TipoTransaccion || (exports.TipoTransaccion = TipoTransaccion = {}));
